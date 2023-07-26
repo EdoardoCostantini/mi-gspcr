979cea847bcdbe9ad78dee5e7062f2235ae4fa80
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-07-26 17:10:26 +0200

    data: finalize missing data checks

--- code/checks/check-missingness.R
+++ code/checks/check-missingness.R
@@ -2,7 +2,7 @@
 # Objective: Describe missing values in the EVS data
 # Author:    Edoardo Costantini
 # Created:   2023-07-21
-# Modified:  2023-07-21
+# Modified:  2023-07-26
 # Notes: 
 
 # Load data --------------------------------------------------------------------
@@ -10,7 +10,7 @@
 EVS <- readRDS("../input/ZA7500_fc_processed.rds")
 EVS <- readRDS("../input/ZA7500_processed.rds")
 
-# Missing data patterns --------------------------------------------------------
+# Proportion of missing values -------------------------------------------------
 
 # Counts of missing values:
 cm <- colSums(is.na(EVS))
@@ -22,19 +22,45 @@ pm <- colMeans(is.na(EVS))
 # Percentage of missing values
 barplot(round(pm, 3) * 100)
 
-# Compute variable-wise count of observed values:
-co <- colSums(!is.na(EVS))
-co
+# > Check Variables with high PM -----------------------------------------------
+pm[pm > .30]
 
-# Compute variable-wise proportions of observed values:
-po <- colMeans(!is.na(EVS))
-po
+# - v173 is high because only asked in European countries (see codebook for list)
+# - v174_LR is high because of high missing rate
+# - v175_LR is high because of high missing rate
 
+# > Check Variables with moderate PM -------------------------------------------
+pm[pm > .10 & pm < .30]
+
+# - all due to real missing values
+# - v276_r data for Great Britain and NL not available
+
+# Missing data patterns --------------------------------------------------------
 
 # Create missing data patterns matrix
-EVS_mdp <- mice::md.pattern(EVS)
+EVS_mdp <- mice::md.pattern(EVS, plot = FALSE)
 
-# Variables with missing values
-sum(EVS_mdp[nrow(EVS_mdp), -ncol(EVS_mdp)] != 0)
+# Number of missing data patterns
+nrow(EVS_mdp) - 1
 
-# Missing data proportion of missing cases
+# Frequency of pattern-wise missing counts:
+table(EVS_mdp[-nrow(EVS_mdp), ncol(EVS_mdp)])
+
+# Covariance coverage ----------------------------------------------------------
+
+# md pairs
+EVS_mdpairs <- mice::md.pairs(EVS)
+
+# Compute covariance coverage matrix:
+cc_mat <- round(EVS_mdpairs$rr / nrow(EVS), 3)*100
+cc_mat
+
+# Extract unique coverage values from the covariance coverage matrix:
+cc <- cc_mat[lower.tri(cc_mat)]
+
+# Obtain the range of the coverage values
+range(cc)
+
+# Find problematic pairs (use entire covariance coverage matrix):
+pat <- cc_mat <= 80
+apply(pat, 2, function(x) names(x)[x])
