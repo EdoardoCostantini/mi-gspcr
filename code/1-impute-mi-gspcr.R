32db79e43b21625a4d2f64c8639ead1244a53f7b
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-08-04 10:55:00 +0200

    feat: prepare trial run of mi-gspcr on small EVS data

---
+++ code/1-impute-mi-gspcr.R
@@ -0,0 +1,81 @@
+# Project:   mi-gspcr-evs
+# Objective: Impute prepared EVS data with Mi-GSPCR
+# Author:    Edoardo Costantini
+# Created:   2023-07-12
+# Modified:  2023-08-04
+# Notes: 
+
+# Load data --------------------------------------------------------------------
+
+# Load EVS data
+EVS <- readRDS("../input/ZA7500_processed.rds")
+EVS <- readRDS("../input/ZA7500_fc_processed.rds") # smaller version
+
+# Load variable types
+var_types <- readRDS("../input/var_types.rds")
+
+# Load imputation methods vector
+imp_meth_vec <- readRDS("../input/imputation_methods-mi-gspcr.rds")
+
+# Ad-hoc data prep -------------------------------------------------------------
+
+# Transform ordinal variables with more than 5 categories to numeric for PCA
+for(j in var_types$ord){
+    if(nlevels(EVS[, j]) >= 5){
+        EVS[, j] <- as.numeric(EVS[, j])
+    }
+}
+
+# Replace v279d_r variable with its log
+plot(density(na.omit(EVS$v279d_r)))
+EVS$v279d_r[!is.na(EVS$v279d_r)] <- log(EVS$v279d_r[!is.na(EVS$v279d_r)])
+EVS$v279d_r <- EVS$v279d_r
+
+# Replace v242 variable with its log
+plot(density(na.omit(EVS$v242)))
+EVS$v242[!is.na(EVS$v242)] <- log(EVS$v242[!is.na(EVS$v242)])
+
+# Store session info -----------------------------------------------------------
+
+# Define run date
+date_time <- format(Sys.time(), "%Y%m%d-%H%M%S")
+
+# Store info about the R session
+R_session <- utils::sessionInfo()
+
+# Save the file in the input folder
+saveRDS(R_session, paste0("../input/", date_time, "-R-session.rds"))
+
+# Imputation -------------------------------------------------------------------
+
+# Parallel MICE ----------------------------------------------------------------
+
+# Parellel MICE run
+mids_mi_gspcr <- futuremice(
+    # Parallel specific arguments
+    parallelseed = 20230804,
+    n.core = 10,
+    # General mice arguments
+    data = EVS_numeric,
+    m = 10,
+    maxit = 50,
+    method = imp_meth_vec,
+    ridge = 0,
+    eps = 0, # bypasses remove.lindep()
+    threshold = 1L,
+    # GSPCR specific arguments
+    thrs = "PR2",
+    fit_measure = "BIC",
+    nthrs = 10,
+    npcs_range = 1:5,
+    K = 1
+)
+
+# Save mids object
+saveRDS(
+    object = mids_mi_gspcr,
+    file = paste0("../output/", date_time, "-mids-mi-gspcr.rds")
+)
+
+# Make a meaningful covergence plot
+plot(imp1)
